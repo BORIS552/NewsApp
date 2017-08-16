a0537fa2a3ee841ac76aef1a28851a9ea4ed5aef
BORIS552 <boris.biswas@gmail.com>
2017-08-16 14:24:02 +0530

    General News Done

--- app/src/main/java/com/example/boris/worldexplorer/fragments/GeneralNewsFragment.java
+++ app/src/main/java/com/example/boris/worldexplorer/fragments/GeneralNewsFragment.java
@@ -23,13 +23,16 @@ import com.example.boris.worldexplorer.fragments.adapters.CustomGridViewAdapter;
 public class GeneralNewsFragment extends Fragment {
 
     private Integer[] thumbGeneralID ={R.drawable.bbc,R.drawable.cnn,R.drawable.guardian,R.drawable.hindu,R.drawable.telegraph,
-            R.drawable.times,R.drawable.bloomberg,R.drawable.google,R.drawable.toi,R.drawable.focus,
+            R.drawable.toi,R.drawable.bloomberg,R.drawable.google,R.drawable.times,R.drawable.focus,
             R.drawable.metro,R.drawable.nytimes};
 
     private String[] newsChannelGeneral = {"BBC NEWS","CNN","GUARDIAN","THE HINDU","THE TELEGRAPH","THE TIMES OF INDIA","BLOOMBERG",
-            "GOOGLE NEWS","TIMES OF INDIA","FOCUS","METRO","NEW YORK TIMES"};
+            "GOOGLE NEWS","TIME","FOCUS","METRO","NEW YORK TIMES"};
 
-    private String[] newsSourceUrl = {"source=bbc-news&sortBy=top&","source=cnn&sortBy=top&"};
+    private String[] newsSourceUrl = {"source=bbc-news&sortBy=top&","source=cnn&sortBy=top&","source=the-guardian-uk&sortBy=top&"
+                                     ,"source=the-hindu&sortBy=top&","source=the-telegraph&sortBy=top&","source=the-times-of-india&sortBy=top&",
+                                      "source=bloomberg&sortBy=top&","source=google-news&sortBy=top&","source=time&sortBy=top&","source=focus&sortBy=top&",
+                                      "source=metro&sortBy=top&","source=the-new-york-times&sortBy=top&"};
 
     //String url = ;
 @Override
--- app/src/main/java/com/example/boris/worldexplorer/fragments/SportsNewsFragment.java
+++ app/src/main/java/com/example/boris/worldexplorer/fragments/SportsNewsFragment.java
@@ -2,6 +2,8 @@ package com.example.boris.worldexplorer.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +23,8 @@ public class SportsNewsFragment extends Fragment {
     private Integer[] thumbSportsID = {R.drawable.espn,R.drawable.espn_cric,R.drawable.fox_sport,R.drawable.football_italia,R.drawable.sports_bible,R.drawable.talk_sport};
 
     private String[] newsChannelSports = {"ESPN","ESPN CRIC INFO","FOX SPORTS","FOOTBALL ITALIA","THE SPORTS BIBLE","TALKSPORT"};
+
+    private String[] newsSourceUrl = {};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View vfrag  = inflater.inflate(R.layout.fragment_sportsnews,container,false);
@@ -30,8 +34,18 @@ public class SportsNewsFragment extends Fragment {
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(getActivity(), "hello" + position,
-                        Toast.LENGTH_SHORT).show();
+
+               // Toast.makeText(getActivity(), "hello" + position,Toast.LENGTH_SHORT).show();
+
+                RecyclerViewFragment recyclerViewFragment = new RecyclerViewFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Bundle args = new Bundle();
+                args.putString("URL", newsSourceUrl[position]);
+                recyclerViewFragment.setArguments(args);
+                fragmentTransaction.replace(R.id.main_container,recyclerViewFragment);
+                fragmentTransaction.commit();
+
             }
         });
 
