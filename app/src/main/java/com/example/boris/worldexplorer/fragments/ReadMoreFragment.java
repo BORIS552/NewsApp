c22ce4d0d24566c5bbf06cb7bc2a857866437e49
BORIS552 <boris.biswas@gmail.com>
2017-09-29 08:39:45 +0530

    UI

--- app/src/main/java/com/example/boris/worldexplorer/fragments/ReadMoreFragment.java
+++ app/src/main/java/com/example/boris/worldexplorer/fragments/ReadMoreFragment.java
@@ -6,9 +6,11 @@ import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.boris.worldexplorer.R;
 
@@ -23,11 +25,25 @@ public class ReadMoreFragment extends Fragment {
         super.onCreate(savedInstanceState);
         View vfrag = inflater.inflate(R.layout.fragment_readmore,container,false);
         WebView webView = (WebView) vfrag.findViewById(R.id.readmore);
+        final ProgressBar pbar = (ProgressBar) vfrag.findViewById(R.id.webProgressBar);
         String url = getArguments().getString("URL");
         webView.setWebViewClient(new WebViewClient());
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.loadUrl(url);
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if(progress < 100 && pbar.getVisibility() == ProgressBar.GONE){
+                    pbar.setVisibility(ProgressBar.VISIBLE);
+
+                }
+
+                pbar.setProgress(progress);
+                if(progress == 100) {
+                    pbar.setVisibility(ProgressBar.GONE);
+                }
+            }
+        });
         return vfrag;
     }
 
